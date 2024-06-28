a4cb9ff7dfcb5f83148103a8e32163006f984edd
Rifqi Akmal <rifqiakmalf@gmail.com>
2024-06-28 20:44:42 +0700

    v1

---
+++ src/App.jsx
@@ -0,0 +1,56 @@
+import Main from './component/Main'
+import About from './component/About'
+import Project from './component/Project';
+import Contact from './component/Contact';
+import './App.css'
+import { useRef } from 'react';
+
+function App() {
+  const mainRef = useRef(null)
+  const aboutRef = useRef(null)
+  const projectRef = useRef(null)
+  const contactRef = useRef(null)
+
+  const scrollToProject = () => {
+    projectRef.current.scrollIntoView({ behavior: 'smooth' })
+  }
+
+  const scrollToMain = () => {
+    mainRef.current.scrollIntoView({ behavior: 'smooth' })
+  }
+
+  const scrollToAbout = () => {
+    aboutRef.current.scrollIntoView({ behavior: 'smooth' })
+  }
+
+  const scrollToContact = () => {
+    contactRef.current.scrollIntoView({ behavior: 'smooth' })
+  }
+
+  return (
+    <>
+      <nav>
+        <img src='/logo.svg' alt='logo' className='logo' onClick={scrollToMain}/>
+        <ul className='nav-button-container'>
+          <li className='nav-button' onClick={scrollToAbout}>About</li>
+          <li className='nav-button' onClick={scrollToProject}>Project</li>
+          <li className='nav-button' onClick={scrollToContact}>Contact</li>
+        </ul>
+      </nav>
+      <section ref={mainRef}>
+        <Main />
+      </section>
+      <section ref={aboutRef} >
+        <About />
+      </section>
+      <section ref={projectRef}>
+        <Project />
+      </section>
+      <section ref={contactRef}>
+        <Contact />
+      </section>
+    </>
+  )
+}
+
+export default App
---
+++ src/assets/data.js
@@ -0,0 +1,29 @@
+export default[
+    {
+        id:"1",
+        title:"Partimer",
+        link:"https://www.figma.com/design/my6lMnJc6SlN0ENG6NFHbj/Partimer-(PPL-II)?node-id=0-1&t=2D393MBSl4gwGYnw-1",
+        description:"Partimer is a website for people that want to search and apply for a part-time job.",
+        img:"/mockup_partimer.png",
+        to:"See Design",
+        type:"UI/UX Design"
+    },
+    {
+        id:"2",
+        title:"Poster",
+        link:"https://postermovie.vercel.app",
+        description:"Poster is a website for people who wants to search up movies they want to watch, it has features such as search and similar movies based on genre.",
+        img:"/mockup_poster.png",
+        to:"Visit Website",
+        type:"Front-end"
+    },
+    {
+        id:"3",
+        title:"Portfolio Website",
+        link:"/",
+        description:"My own portfolio website displaying about myself, projects that I had made/build, and also ways to reach me",
+        img:"/mockup_portfolio.png",
+        to:"Visit Website",
+        type:"Front-end"
+    }
+]
